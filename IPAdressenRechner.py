09980b8d6d72250a0689ea6e521e9665350c3cab
Mef <122865060+Mef01@users.noreply.github.com>
2024-01-25 08:58:52 +0100

    Implemntet function

---
+++ IPAdressenRechner.py
@@ -0,0 +1,12 @@
+def Checkinput(IPv4):
+    print('')
+
+
+def IP4_to_Binary(IPv4):
+
+
+    print('')
+
+IP4Adresse = input('Geben sie bitte ihre IPv4 adresse ein.') 
+SubnetmaskAdresse = input ('Geben sie bitte ihre Subnetmask ein')
+    
