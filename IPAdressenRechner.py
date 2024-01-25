260053e3abed500efd40058eaa2a91647cfe694d
Mef <122865060+Mef01@users.noreply.github.com>
2024-01-25 09:19:44 +0100

    Checkinput for lenght

--- IPAdressenRechner.py
+++ IPAdressenRechner.py
@@ -1,12 +1,13 @@
 def Checkinput(IPv4):
-    print('')
-
-
+    if len(IPv4) > 15: 
+        return False
+    
 def IP4_to_Binary(IPv4):
 
 
     print('')
 
 IP4Adresse = input('Geben sie bitte ihre IPv4 adresse ein.') 
+Checkinput(IP4Adresse)
 SubnetmaskAdresse = input ('Geben sie bitte ihre Subnetmask ein')
-    
+Checkinput(SubnetmaskAdresse)  
